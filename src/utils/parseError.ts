6992db2767f7f4188acf36cf9ad25043672dc849
Tomáš Sabol <tomassabol2@gmail.com>
2023-12-05 11:41:42 +0100

    improve error handling function

--- src/utils/parseError.ts
+++ src/utils/parseError.ts
@@ -1,16 +1,14 @@
 import { AxiosError } from "axios";
-import { z } from "zod";
+import { ZodError } from "zod";
 
+/**
+ *
+ */
 export function parseError(e: unknown) {
-  if (e instanceof z.ZodError) {
-    return e.issues.map((issue) => {
-      return {
-        path: issue.path,
-        message: issue.message,
-      };
-    });
+  if (e instanceof ZodError) {
+    return e.format();
   } else if (e instanceof AxiosError) {
-    return e.response?.data;
+    return e.response?.data as unknown;
   } else if (e instanceof Error) {
     return e.message;
   } else {
